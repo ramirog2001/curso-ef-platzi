f0da9e06832c511d88a9386db23ca7481d588104
ramiro <ramirogalende@gmail.com>
2023-12-18 14:38:25 -0300

    Agrego postgresql como base de datos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using curso_ef_platzi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddNpgsql<TareasContext>(builder.Configuration.GetConnectionString("connectionTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/isup", async ([FromServices] TareasContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos creada: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
--- TareasContext.cs
+++ TareasContext.cs
@@ -7,5 +7,5 @@ public class TareasContext : DbContext
 {
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Tarea> Tareas { get; set; }
-    public TareasContext(DbContextOptions options){}
+    public TareasContext(DbContextOptions options) : base(options){}
 }
