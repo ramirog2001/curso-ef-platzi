0fc893080acd6db41c52ca593efda9bc25dc6b7d
Ramiro Galende <ramirogalende@gmail.com>
2023-12-20 22:03:47 -0300

    Agrego codigo para actualizar entidades

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.MapGet("/isup", async ([FromServices] TareasContext dbContext) => {
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
-    return Results.Ok(dbContext.Tareas.Include(x => x.Categoria).Select(x => new{ x.Titulo, x.Descripcion, x.Categoria, x.PrioridadTarea })); //.Include(x => x.Categoria).ToList());
+    return Results.Ok(dbContext.Tareas.Include(x => x.Categoria).Select(x => new{ x.TareaId ,x.Titulo, x.Descripcion, x.Categoria, x.PrioridadTarea })); //.Include(x => x.Categoria).ToList());
 });
 
 app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
@@ -30,5 +30,27 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
 
     return Results.Ok($"Se han modificado {filasModificadas} filas");
 });
+
+app.MapPut("/api/tareas/{idTarea}",
+    async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid idTarea) =>
+    {
+        var tareaOld = dbContext.Tareas.Find(idTarea);
+        
+        if (tareaOld is null)
+            return Results.BadRequest("No se encontro una tarea con el id requerido");
+
+        tarea.TareaId = tareaOld.TareaId;
+        tarea.FechaCreacion = tareaOld.FechaCreacion;
+        var props = tareaOld.GetType().GetProperties();
+        foreach (var prop in tareaOld.GetType().GetProperties())
+        {
+            if(prop.CanWrite)
+                prop.SetValue(tareaOld, prop.GetValue(tarea));
+        }
+
+        var rowsChanged = await dbContext.SaveChangesAsync();
+
+        return Results.Ok($"Se han cambiado {rowsChanged} filas");
+    });
 
 app.Run();
